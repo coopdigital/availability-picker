9e76a8bfc143010d414578e9928ef7228dd8f26a
Simon Stead <simonstead@me.com>
2018-03-07 12:05:21 +0000

    add cors

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
 
 def quantity(sku):
     return 3
 
+
 @app.route('/availability/<sku>', methods=['GET'])
 def availability(sku):
-    data = dict(
-        sku=sku,
-        quantity=quantity(sku)
-    )
+    data = dict(sku=sku, quantity=quantity(sku))
     return jsonify(data), 200
